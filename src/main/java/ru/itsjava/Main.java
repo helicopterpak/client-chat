0114131dc6146ef89829ce81127f995f7f2f960d
Pak <helicopterpak@gmail.com>
2022-11-21 17:38:17 +0300

    new lesson

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,10 +1,12 @@
 package ru.itsjava;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Main {
+public class Main  {
     public final static int PORT = 8081;
     public final static String HOST = "localhost";
     public static void main(String[] args) throws IOException {
@@ -14,6 +16,11 @@ public class Main {
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
             serverWriter.println("Hi from client!");
             serverWriter.flush();
+
+            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while(true) {
+                System.out.println(serverReader.readLine());
+            }
         }
     }
 }
