484a868a84d51b079be28e6cf56879e34e53bcdf
Pak <helicopterpak@gmail.com>
2022-12-02 14:11:26 +0300

    new task

---
+++ src/main/java/ru/itsjava/services/ClientServiceImpl.java
@@ -0,0 +1,36 @@
+package ru.itsjava.services;
+
+import lombok.SneakyThrows;
+import ru.itsjava.services.ClientService;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientServiceImpl implements ClientService {
+    public final static int PORT = 8081;
+    public final static String HOST = "localhost";
+
+    @SneakyThrows
+    @Override
+    public void start() {
+        Socket socket = new Socket(HOST, PORT);
+
+        if (socket.isConnected()) {
+            new Thread(new SocketRunnable(socket)).start();
+
+            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
+
+            MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
+
+            System.out.println("Print message");
+            String consoleMessage = messageInputService.getMessage();
+
+            serverWriter.println(consoleMessage);
+            serverWriter.flush();
+
+
+        }
+    }
+}
---
+++ src/main/java/ru/itsjava/services/SocketRunnable.java
@@ -0,0 +1,20 @@
+package ru.itsjava.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.net.Socket;
+
+@RequiredArgsConstructor
+public class SocketRunnable implements Runnable{
+    private final Socket socket;
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        MessageInputServiceImpl serverReader = new MessageInputServiceImpl(socket.getInputStream());
+        while(true) {
+            System.out.println(serverReader.getMessage());
+        }
+    }
+}
